44b5928bb1df41d1f00cafc5aa313297dc9ff025
yousefrafat <y_alkarnawi@outlook.com>
2025-05-09 01:43:57 +0300

    Update app.py

--- app/app.py
+++ app/app.py
@@ -83,6 +83,7 @@ def hello_world():
             <tr><td>visits (this IP)</td><td>{ip_visits}</td></tr>
             <tr><td>total visits</td><td>{total_visits}</td></tr>
         </table>
+        <h1>MAJD JAMA'AH</h1>
     </body>
     </html>
     """
